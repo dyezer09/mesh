3b81f0d86f31944104379bfdba6ea791c7bf2c30
dyezer09 <den07y@gmail.com>
2024-05-05 16:03:19 +0300

    changed: products

--- src/app.js
+++ src/app.js
@@ -21,38 +21,116 @@ console.log(__dirname)
 
 
 app.engine('.hbs', engine({
-	extname: '.hbs',
-	defaultLayout: 'main',
-	layoutsDir: path.resolve(__dirname, './views/layouts'),
-	partialsDir: path.resolve(__dirname, './views/partials')
-  }));
-  app.set('view engine', 'hbs');
-  app.set('views', path.resolve(__dirname, './views'));
-  app.use(express.static(path.join(__dirname, "")))
+  extname: '.hbs',
+  defaultLayout: 'main',
+  layoutsDir: path.resolve(__dirname, './views/layouts'),
+  partialsDir: path.resolve(__dirname, './views/partials')
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.resolve(__dirname, './views'));
+app.use(express.static(path.join(__dirname, "")))
+
+
+
+let names1 = [
+  {
+    name: '1',
+    cost: '9'
+  },
+  {
+    name: '2',
+    cost: '9'
+  },
+  {
+    name: '3',
+    cost: '9'
+  },
+  {
+    name: '4',
+    cost: '9'
+  },
+  {
+    name: '5',
+    cost: '9'
+  },
+  {
+    name: '6',
+    cost: '9'
+  }
+];
+let names2 = [
+  {
+    name: '1',
+    cost: '9'
+  },
+  {
+    name: '2',
+    cost: '9'
+  },
+  {
+    name: '3',
+    cost: '9'
+  },
+  {
+    name: '4',
+    cost: '9'
+  },
+  {
+    name: '5',
+    cost: '9'
+  },
+  {
+    name: '6',
+    cost: '9'
+  }
+];
+let names3 = [
+  {
+    name: '1',
+    cost: '9'
+  },
+  {
+    name: '2',
+    cost: '9'
+  },
+  {
+    name: '3',
+    cost: '9'
+  },
+  {
+    name: '4',
+    cost: '9'
+  },
+  {
+    name: '5',
+    cost: '9'
+  },
+  {
+    name: '6',
+    cost: '9'
+  }
+];
+
+
+
+app.get("/", (req, res) => {
+
+  //res.render("pages/index", { layout: "main"});
+  res.render("pages/empty", { layout: "origin", names1, names2, names3 });
+});
+app.get("/c", (req, res) => {
+
+  //res.render("pages/index", { layout: "main"});
+  res.render("pages/empty", { layout: "contact" });
+});
 
 
 
 
 
-
-  app.get("/", (req, res) => {
-	
-	//res.render("pages/index", { layout: "main"});
-  res.render("pages/empty", { layout: "origin"});
-  });
-  app.get("/c", (req, res) => {
-	
-    //res.render("pages/index", { layout: "main"});
-    res.render("pages/empty", { layout: "contact"});
-    });
-
-
-
-
-
-  app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 
 
